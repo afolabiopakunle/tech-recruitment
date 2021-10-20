5fdf86646407bd0218968658ce3f39b49d8b3da1
Afolabi Opakunle <opakunleafolabi@gmail.com>
2021-10-20 08:52:22 +0100

    done for push

--- src/app/success-stories/success-stories.component.ts
+++ src/app/success-stories/success-stories.component.ts
@@ -43,20 +43,20 @@ export class SuccessStoriesComponent implements OnInit {
   stories: StoryModel[] = [
     {
       story: 'Ever since I joined kaiglo, I have experienced improved sales due to their market insight data.',
-      owner: 'Afolabi Opakunle',
+      owner: 'Afolabi',
       imageUrl: '../../assets/images/success-stories/baby-boss-1.png',
-      role: 'CE0 Errands Hub'
+      role: 'CEO Errands Hub'
     },
     {
       story: 'Software has been employed by our firm for a couple of years now. The workings around it are pates',
-      owner: 'Emmanuel',
-      imageUrl: '../../assets/images/success-stories/baby-boss-2.jpg',
+      owner: 'Esther',
+      imageUrl: '../../assets/images/success-stories/baby-boss-2.png',
       role: 'GMD Sterling Bank'
     },
     {
       story: 'HumanManager has made our payroll processing very seamless and efficient. It is easy to understand and use.',
-      owner: 'Nelson Eboghade',
-      imageUrl: '../../assets/images/success-stories/baby-boss-1.png',
+      owner: 'Nelson',
+      imageUrl: '../../assets/images/success-stories/image.png',
       role: 'Customer Experience'
     },
   ]
